64818409cbe34443834a0159c165f3186840e7e2
michaelhsia <michaelhsia8716@gmail.com>
2024-01-25 14:57:57 +0800

    add login error alert and index.html div tag to template tag

--- assets/js/login.js
+++ assets/js/login.js
@@ -21,25 +21,36 @@ const app = createApp({
   },
   methods: {
     loginPost() {
-      axios.post(`${url}/admin/signin`, this.user).then((res) => {
-        // 把回傳的 token 及 expired timestamp 用解構賦值方式存成同名變數
-        const { token, expired } = res.data;
+      axios
+        .post(`${url}/admin/signin`, this.user)
+        .then((res) => {
+          // 把回傳的 token 及 expired timestamp 用解構賦值方式存成同名變數
+          const { token, expired } = res.data;
 
-        // 把 token 及 expired 存到 cookie
-        document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+          // 把 token 及 expired 存到 cookie
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
 
-        // 登入成功通知
-        Swal.fire({
-          title: "登入成功",
-          icon: "success",
-          showConfirmButton: false,
-          timer: 1000,
+          // 登入成功通知
+          Swal.fire({
+            title: "登入成功",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+
+          // 把資料存到 cookie 後，頁面導到商品頁
+          // 用 setTimeout 等通知跑完再跳商品頁
+          setTimeout(() => (location.href = "index.html"), 1500);
+        })
+        .catch((err) => {
+          // 登入失敗通知
+          Swal.fire({
+            title: "登入失敗",
+            icon: "warning",
+            showConfirmButton: false,
+            timer: 1000,
+          });
         });
-
-        // 把資料存到 cookie 後，頁面導到商品頁
-        // 用 setTimeout 等通知跑完再跳商品頁
-        setTimeout(() => (location.href = "index.html"), 1500);
-      });
     },
   },
 });
